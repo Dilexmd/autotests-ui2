02e357c24b24af826c6a21a9511b8b5744a6cde3
Dilexmd <lexmd_work@mail.ru>
2026-01-20 14:25:11 +0300

    Моки и оптимизация UI автотестов

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,6 +2,8 @@ import allure
 from playwright.sync_api import Playwright, Page
 
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
     playwright: Playwright,
@@ -20,7 +22,9 @@ def initialize_playwright_page(
             snapshots=True,
             sources=True
         )
+
         page = context.new_page()
+        mock_static_resources(page)
 
         yield page
 
